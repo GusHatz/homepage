56b6a93c2d617b6d6d6900ac3c5df42c8d5c07d3
Konstantinos Hatzopoulos <106545434+GusHatz@users.noreply.github.com>
2022-07-09 15:21:05 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let my_gh_button = document.querySelector("#button_my_gh");
+    my_gh_button.addEventListener("click", function() {
+        window.open("https://github.com/GusHatz", "_blank");
+    })
+});
